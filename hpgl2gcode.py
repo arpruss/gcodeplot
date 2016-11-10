48cc676178057cc7e1f32db6ab8c8b0141ae30ef
arpruss <arpruss@gmail.com>
2016-11-10 00:09:14 -0600

    arguments

--- hpgl2gcode.py
+++ hpgl2gcode.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python
 import re
 import sys
+import getopt
 
 SCALE_NONE = 0
 SCALE_DOWN_ONLY = 1
@@ -34,10 +36,13 @@ class Plotter(object):
         
 
 class Scale(object):
-    def __init__(self, offset=(0.,0.), scale=(0.,0.)):
+    def __init__(self, scale=(1.,1.), offset=(0.,0.)):
         self.offset = offset
         self.scale = scale
         
+    def __repr__(self):
+        return str(self.scale)+','+str(self.offset)
+
     def fit(self, plotter, xyMin, xyMax):
         s = [0,0]
         o = [0,0]
@@ -85,12 +90,12 @@ def dedup(commands):
 
     return newCommands
         
-def emitGcode(commands, scale = Scale(), plotter=Plotter(), autoScale=SCALE_NONE, pause = False):
+def emitGcode(commands, scale = Scale(), plotter=Plotter(), scalingMode=SCALE_NONE, pause = False):
 
     xyMin = [float("inf"),float("inf")]
     xyMax = [float("-inf"),float("-inf")]
     
-    allFit = False
+    allFit = True
     
     for c in commands:
         if c.point is not None:
@@ -100,11 +105,11 @@ def emitGcode(commands, scale = Scale(), plotter=Plotter(), autoScale=SCALE_NONE
                 xyMin[i] = min(xyMin[i], c.point[i])
                 xyMax[i] = max(xyMax[i], c.point[i])
     
-    if autoScale == SCALE_NONE:
+    if scalingMode == SCALE_NONE:
         if not allFit:
             sys.stderr.write("Drawing out of range.")
             return None
-    elif autoScale != SCALE_DOWN_ONLY or not allFit:
+    elif scalingMode != SCALE_DOWN_ONLY or not allFit:
         if xyMin[0] > xyMax[0]:
             sys.stderr.write("No points.")
             return None
@@ -144,20 +149,25 @@ def emitGcode(commands, scale = Scale(), plotter=Plotter(), autoScale=SCALE_NONE
         gcode.append('G0 Z%.3f (pen up)' % plotter.penUpZ)
     return ('\n@pause\n' if pause else '\n').join(gcode)
     
-def parseHPGL(file):
+def parseHPGL(file,dpi=(1016.,1016.)):
+    try:
+        scale = (254./dpi[0], 254./dpi[1])
+    except:
+        scale = (254./dpi, 254./dpi)
+
     commands = []
     with open(file, 'r') as f:
         for cmd in re.sub(r'\s',r'',f.read()).split(';'):
             if cmd.startswith('PD'):
                 try:
                     x,y = map(float, cmd[2:].split(',',2))
-                    commands.append(Command(Command.MOVE_PEN_DOWN, point=(x/40., y/40.)))
+                    commands.append(Command(Command.MOVE_PEN_DOWN, point=(x*scale[0], y*scale[1])))
                 except:
                     commands.append(Command(Command.MOVE_PEN_DOWN))
             elif cmd.startswith('PU'):
                 try:
                     x,y = map(float, cmd[2:].split(',',2))
-                    commands.append(Command(Command.MOVE_PEN_UP, point=(x/40., y/40.)))
+                    commands.append(Command(Command.MOVE_PEN_UP, point=(x*scale[0], y*scale[1])))
                 except:
                     commands.append(Command(Command.MOVE_PEN_UP))
             elif cmd.startswith('IN'):
@@ -167,8 +177,55 @@ def parseHPGL(file):
     return commands
     
 if __name__ == '__main__':
-    plotter = Plotter(xyMin=(60.,20.),xyMax=(160.,120.))
-    drawing = emitGcode(dedup(parseHPGL(sys.argv[1])), autoScale=SCALE_BEST, pause=False, plotter=plotter)
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "rfdma:D:", ["--allow-repeats", "--scale-to-fit",
+                        "--scale-down", "--scale-manual", "--area=", '--input-dpi='], )
+        if len(args) != 1:
+            raise getopt.GetoptError("invalid commandline")
+
+        doDedup = True    
+        scale = Scale()
+        scalingMode = SCALE_BEST
+        plotter = Plotter(xyMin=(60.,20.),xyMax=(160.,120.))
+        dpi = (1016., 1016.)
+            
+        for opt,arg in opts:
+            if opt in ('-r','--allow-repeats'):
+                doDedup = False
+            elif opt in ('-f','--scale-to-fit'):
+                scalingMode = SCALE_BEST
+            elif opt in ('-d','--scale-down'):
+                scalingMode = SCALE_DOWN_ONLY
+            elif opt in ('-m','--scale-manual'):
+                scalingMode = SCALE_NONE
+            elif opt in ('-a','--area'):
+                v = map(float, arg.split(','))
+                plotter.xyMin = (v[0],v[1])
+                plotter.xyMax = (v[2],v[3])
+            elif opt in ('-D','--input-dpi'):
+                v = map(float, arg.split(','))
+                if len(v) > 1:
+                    dpi = v[0:2]
+                else:
+                    dpi = (v[0],v[0])
+        
+    except getopt.GetoptError:
+        print("hpgl2gcode.py [options] inputfile > output.gcode")
+        print("""
+ -r|--allow-repeats: do not deduplicate paths [default: off]
+ -f|--scale-to-fit: scale to fit plotter area [default]
+ -d|--scale-down: scale to fit plotter area only if too big
+ -m|--scale-manual: no scaling
+ -a|--area=x1,y1,x2,y2: print area in millimeters [default: 0,0,200,200]
+ -D|--input-dpi=xdpi[,ydpi]: hpgl dpi
+""")
+        sys.exit(2)
+        
+    commands = parseHPGL(args[0], dpi=dpi)
+    if doDedup:
+        commands = dedup(commands)
+        
+    drawing = emitGcode(dedup(commands), scale=scale, scalingMode=scalingMode, pause=False, plotter=plotter)
     if drawing is not None:
         print(drawing)
 
